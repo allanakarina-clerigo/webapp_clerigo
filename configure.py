0638f672e9efc17f8d058285682e540577636946
allanakarina.clerigo <allanakarina.clerigo@g.msuiit.edu.ph>
2025-05-29 14:57:21 +0800

    added configures

---
+++ configure.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+SECRET_KEY = getenv('any_string_will_do')
+DB_HOST = getenv("localhost")
+DB_USER = getenv("root")
+DB_PASSWORD = getenv("akclerigo")
+DB_DATABASE = getenv("database")
+
+CLOUD_NAME = getenv('dkc0twhfx')
+API_KEY = getenv('984899481765332')
+API_SECRET = getenv('sQakvKDkWjMek-GmEk1iNma_w-o')
